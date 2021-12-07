8b2820910bffae7d7faa1a4a6f1a569615ab8aa6
Abel Hlongwani <abel.hlongwani@integrove.com>
2021-12-07 22:44:58 +0200

    Final Project done 2.0

--- src/components/CorouselComponent.js
+++ src/components/CorouselComponent.js
@@ -19,7 +19,7 @@ const CorouselComponent = () => {
                 <Carousel.Item>
                     <img
                     className="d-block w-100"
-                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OUmeZkyCO70X4qUiNgzedAHaDt%26pid%3DApi&f=1"
+                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-hMM7liTVslytO6-xqBYlAHaEv%26pid%3DApi&f=1"
                     alt="Second slide"
                     />
 
